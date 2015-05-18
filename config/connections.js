06167c1f745166c1e6328cd0b1cd1b489a66074f
solskido <solskido@gmail.com>
2015-05-18 00:52:36 -0400

    Switch mongo connection back

--- config/connections.js
+++ config/connections.js
@@ -23,6 +23,10 @@ module.exports.connections = {
 
 	"tomeMongoLab": {
 		"adapter": "sails-mongo",
-		"url": process.env.DB_URL
+		"host": process.env.DB_HOST,
+		"port": process.env.DB_PORT,
+		"database": process.env.DB_ROOT,
+		"user": process.env.DB_USER,
+		"password": process.env.DB_PASSWORD
 	}
 };
