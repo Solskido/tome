71a1135713891d02889727700437e6b5a4c6245f
solskido <solskido@gmail.com>
2015-05-27 21:59:54 -0400

    Added a description to campaigns, and added an auto-incrementing tag suffix to prevent duplicates.

--- api/models/Campaigns.js
+++ api/models/Campaigns.js
@@ -19,6 +19,9 @@ module.exports = {
 			"type": "string",
 			"unique": true
 		},
+		"description": {
+			"type": "string"
+		},
 		"rooms": {
 			"collection": "Rooms",
 			"via": "campaign"
@@ -45,6 +48,52 @@ module.exports = {
 	"beforeCreate": function(attrs, next)
 	{
 		attrs.tag = attrs.name.trim().replace(/[^A-Za-z ]/g, '').replace(/ /g, '-').toLowerCase();
-		next();
+		Campaigns.find({
+			"tag": {
+				"contains": attrs.tag
+			}
+		}).exec(function(err, existingCampaigns)
+		{
+			if(err)
+			{
+				sails.log.error(err);
+			}
+			else if(!existingCampaigns.length)
+			{
+				next();
+			}
+			else
+			{
+				function incrementTagIndex(tag)
+				{
+					var lastHyphen = _.lastIndexOf(tag, "-");
+					if(lastHyphen >= 0)
+					{
+						var index = parseInt(tag.substr(lastHyphen + 1));
+						if(_.isNaN(index))
+						{
+							tag += "-2";
+						}
+						else
+						{
+							tag = (tag.substr(0, lastHyphen) + "-" + (index + 1));
+						}
+					}
+					else
+					{
+						tag += "-2";
+					}
+
+					return tag;
+				}
+
+				while(_.find(existingCampaigns, { "tag": attrs.tag }))
+				{
+					attrs.tag = incrementTagIndex(attrs.tag);
+				}
+
+				next();
+			}
+		});
 	}
 };
